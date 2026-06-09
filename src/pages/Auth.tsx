9ccc662c858655d1e6c88f5577f1f99f254d1abc
Anton Kamil <tantony31710@gmail.com>
2026-06-10 00:48:17 +0300

    Update Auth.tsx

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
-import { supabase } from '@/lib/supabase'
+import { supabase } from '../lib/supabase';
 
 export function Auth() {
   const navigate = useNavigate()
