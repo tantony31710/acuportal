c26162e6d635f9b199f8a3e969704baa0a491349
Anton Kamil <tantony31710@gmail.com>
2026-06-11 01:24:41 +0300

    Update Auth.tsx

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -2,7 +2,7 @@ import { useEffect, useState } from 'react'
 import { useNavigate, Link } from 'react-router-dom'
 import { supabase } from '../lib/supabase';
 
-export function Auth() {
+export default function Auth() {
   const navigate = useNavigate()
   const [tab, setTab] = useState<'signin'|'signup'>('signin')
   const [email, setEmail] = useState('')
